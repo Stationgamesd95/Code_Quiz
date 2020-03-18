55e52368d0e693107d9e8a4e97d3296675730590
Desai <shivan.s.desai@gmail.com>
2020-03-18 09:22:09 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+var startBtn = document.getElementById("#startBtn");
+var quizContainer = document.getElementById("#container");
+
+
+var questions = [
+    {
+      title: "When was Javascript made?",
+      choices: ["2000", "1989", "1995", "1993"],
+      answer: "1995"
+    },
+    {
+      title: "When was the first website created?",
+      choices: ["1991", "1987", "1992", "1988"],
+      answer: "1991"
+    },
+   
+        {
+          title: "How many foundations are for web development?",
+          choices: ["4", "1", "2", "3"],
+          answer: "3"
+        },
+        {
+          title: "What is CSS for?",
+          choices: ["Writing", "Interacting", "Decorating", "Both"],
+          answer: "Decorating"
+        },
+          {
+          title: "Which Web Development Bootcamp is the best?",
+          choices: ["General Assembly", "UCF", "Springboard", "Thinkful"],
+          answer: "UCF"
+        }
+      ];
+
+
